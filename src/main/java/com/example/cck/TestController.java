72a49e1f4922c86cb865d5ebe4664c234d858d7b
rpppprp <rpppprp13@gmail.com>
2025-12-03 14:20:36 +0900

    테스트->인덱스로 수정

--- src/main/java/com/example/cck/TestController.java
+++ src/main/java/com/example/cck/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
 
     // 브라우저에서 http://localhost:9090/test 로 접속하면 이 메서드가 실행됩니다.
-    @GetMapping("/test")
+    @GetMapping("/index")
     public String showTestPage(Model model) {
 
         // 1. Model 객체를 사용하여 HTML 템플릿으로 데이터를 전달합니다.
@@ -16,6 +16,6 @@ public class TestController {
         model.addAttribute("greeting", "안녕하세요! 컨트롤러에서 전달된 메시지입니다.");
 
         // 2. 리턴 값 "testpage"는 templates 폴더 아래의 testpage.html 파일을 찾으라는 의미입니다.
-        return "testpage";
+        return "index";
     }
 }
